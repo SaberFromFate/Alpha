abd7e691c26c8920e1dc6db89570ff5b41c7d898
SaberFromFate <78263741+SaberFromFate@users.noreply.github.com>
2021-03-04 15:27:24 +0300

    Add files via upload

---
+++ Samosval.ino
@@ -0,0 +1,15 @@
+#imclude <Servo.h>
+
+B1 = A0;
+B2 = A1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Слава Кайзеру");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
