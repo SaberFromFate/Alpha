c5d324f13cb54615222d907d66bcc74e912d6789
SaberFromFate <78263741+SaberFromFate@users.noreply.github.com>
2021-03-06 14:23:24 +0300

    Update Samosval.ino

--- Samosval.ino
+++ Samosval.ino
@@ -1,14 +1,34 @@
 #include <Servo.h>
 
-B1 = A0;
+Servo SD;
+
+int B1       = A0;
+
+int PIN_ECHO = 11;
+int PIN_TRIG = 12;
+
+bool state = 0;
+
+void dist() {
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(PIN_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG, LOW);
+  duration = pulseIn(PIN_ECHO, HIGH);
+  cm = (duration / 2) / 29.1;
+  if cm <= 30{
+    state = 1;
+  }
+  delay(100);
+}
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Слава Кайзеру");
+  SD.attach(20);
+  SD.write(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
