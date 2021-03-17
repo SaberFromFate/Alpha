0008f80f4b828c942cf0136fb8cef5a3d9e5d725
SaberFromFate <78263741+SaberFromFate@users.noreply.github.com>
2021-03-17 11:39:56 +0300

    Create Beackon.ino

---
+++ Beackon.ino
@@ -0,0 +1,21 @@
+int L   = 2;
+int val = 0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  while(!Serial.avaible){
+    delay(100);
+  }
+  if(val == 0){
+    tone(L, 0);
+  }
+  if(val == 1){
+    tone(L, 40000);
+    delay(250);
+    notone(L);
+    delay(250);
+  }
+}
